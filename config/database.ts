a6a2853a0481ec5f695bf40f44ae6c60b5893601
HidanZZ <aitdaoudelhoussein@gmail.com>
2023-09-15 20:56:14 +0100

    fix env

--- config/database.ts
+++ config/database.ts
@@ -1,16 +1,17 @@
-import config from "config";
 import { connect } from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
 const connectDB = async () => {
-  try {
-    const mongoURI: string = config.get("mongoURI");
-    await connect(mongoURI);
-    console.log("MongoDB Connected...");
-  } catch (err) {
-    console.error(err.message);
-    // Exit process with failure
-    process.exit(1);
-  }
+	try {
+		const mongoURI: string = process.env.MONGO_URI;
+		await connect(mongoURI);
+		console.log("MongoDB Connected...");
+	} catch (err) {
+		console.error(err.message);
+		// Exit process with failure
+		process.exit(1);
+	}
 };
 
 export default connectDB;
